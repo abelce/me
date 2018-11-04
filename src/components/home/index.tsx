f5da934b357baef9421578b8fe8f3cd84832fe84
tzx <tzx@tzx.local>
2018-11-04 19:46:50 +0800

    添加prettier

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as Style from './style';
+import * as Style from './style.scss';
 import cn from 'classnames';
 
 const Links = [
@@ -7,14 +7,17 @@ const Links = [
     id: 'blog',
     label: '博客',
     link: '//blog.tangzhengxiong.com',
-  },{
+  },
+  {
     id: 'github',
     label: 'github',
     link: 'https://github.com/abelce',
-  }, {
+  },
+  {
     id: 'resume',
     label: 'resume',
-    link: 'https://drive.google.com/file/d/1pqgmvYou99Cj35AOa10vFHGxA_zGdg8o/view?ths=true',
+    link:
+      'https://drive.google.com/file/d/1pqgmvYou99Cj35AOa10vFHGxA_zGdg8o/view?ths=true',
   },
 ];
 
@@ -30,10 +33,7 @@ const kills = [
   'webpack',
 ];
 
-const extend = [
-  'golang',
-  'docker'
-];
+const extend = ['golang', 'docker'];
 
 class Home extends React.Component {
   render() {
@@ -50,47 +50,34 @@ class Home extends React.Component {
           <section>
             <h5>技术栈</h5>
             <ul>
-              {
-                kills.map(kill => (
-                  <li>
-                    {kill}
-                  </li>
-                ))
-              }
+              {kills.map(kill => (
+                <li>{kill}</li>
+              ))}
             </ul>
           </section>
           <section>
             <h5>涉略</h5>
             <ul>
-              {
-                extend.map(ex => (
-                  <li>
-                    {ex}
-                  </li>
-                ))
-              }
+              {extend.map(ex => (
+                <li>{ex}</li>
+              ))}
             </ul>
           </section>
         </aside>
         <div className={Style.container}>
           <ul className={Style.links}>
-            {
-              Links.map((item, index) => (
-                <li>
-                   <a href={item.link} target="_blank">
-                     <div className={Style[`link${index + 1}`]}>
-                        {item.label}
-                     </div>
-                   </a>
-                </li>
-              ))
-            }
+            {Links.map((item, index) => (
+              <li>
+                <a href={item.link} target="_blank">
+                  <div className={Style[`link${index + 1}`]}>{item.label}</div>
+                </a>
+              </li>
+            ))}
           </ul>
-          <footer>
-          </footer>
+          <footer />
         </div>
       </div>
-    )
+    );
   }
 }
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -1,121 +1,130 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 const hostMap = {
   development: '"http://localhost:3001"',
 };
 
 const config = {
-  mode: isDev() ? "development" : "production",
-  devtool: isDev() ? "cheap-module-eval-source-map" : false,
+  mode: isDev() ? 'development' : 'production',
+  devtool: isDev() ? 'cheap-module-eval-source-map' : false,
   devServer: {
     disableHostCheck: true,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     hot: true,
     port: 3011,
-    contentBase: __dirname + "/dist",
+    contentBase: __dirname + '/dist',
     historyApiFallback: true,
   },
   entry: {
     vendor: ['react', 'react-dom', 'react-router-dom'],
-    app: __dirname + "/src/app.tsx",
+    app: __dirname + '/src/app.tsx',
     // po: ["./src/assets/i18ns/zh_CN.po", "./src/assets/i18ns/en_US.po"],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: `${process.env.PUBLIC || "/"}`,
-    filename: isDev() ? "[name].[hash].js" : "[name].[chunkhash].js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: `${process.env.PUBLIC || '/'}`,
+    filename: isDev() ? '[name].[hash].js' : '[name].[chunkhash].js',
   },
+  // eslint: {
+  //   configFile: './.eslintrc'
+  // },
   module: {
     rules: [
       {
         test: /zh_CN.po$/,
-        loader: "expose-loader?zh_CN!json-loader!po-loader",
+        loader: 'expose-loader?zh_CN!json-loader!po-loader',
       },
       {
         test: /en_US.po$/,
-        loader: "expose-loader?en_US!json-loader!po-loader",
+        loader: 'expose-loader?en_US!json-loader!po-loader',
       },
+      // {
+      //   test: /\.jsx$|\.js$|\.tsx$|\.ts$/,
+      //   include: /src/,
+      //   exclude: [/node_modules/, /src\/assets/],
+      //   enforce: 'pre',
+      //   use: [{
+      //       loader: 'eslint-loader',
+      //       options: { fix: true }
+      //   }]
+      // },
       {
         test: /\.jsx$|\.js$|\.tsx$|\.ts$/,
         include: /src/,
         exclude: [/node_modules/, /src\/assets/],
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg|pdf|jpg)$/,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
-          name: "[path][name].[hash].[ext]",
+          name: '[path][name].[hash].[ext]',
           limit: 8192,
-          context: "src",
-          fallback: "file-loader",
+          context: 'src',
+          fallback: 'file-loader',
         },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
         loader: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "typings-for-css-modules-loader",
+            loader: 'typings-for-css-modules-loader',
             options: {
               modules: true,
               namedExport: true,
               camelCase: true,
               minimize: true,
-              localIdentName: "[local]_[hash:base64:5]",
+              localIdentName: '[local]_[hash:base64:5]',
             },
           },
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
-              ident: "postcss",
+              ident: 'postcss',
               plugins: function() {
-                return [require("autoprefixer")];
+                return [require('autoprefixer')];
               },
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: __dirname + "/src/assets/index.ejs",
+      filename: 'index.html',
+      template: __dirname + '/src/assets/index.ejs',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
     new webpack.DefinePlugin({
       __ENV__: JSON.stringify(process.env.NODE_ENV),
       __: function(k) {
         return k;
       },
-      _pms_host: hostMap[process.env.NODE_ENV] || "",
+      _pms_host: hostMap[process.env.NODE_ENV] || '',
     }),
     new webpack.HashedModuleIdsPlugin(),
   ],
   resolve: {
-    modules: ["src", "node_modules"],
-    extensions: [".js", ".tsx", ".ts", ".scss"],
+    modules: ['src', 'node_modules'],
+    extensions: ['.js', '.tsx', '.ts', '.scss'],
   },
   optimization: {
     minimize: isDev() ? false : true,
     splitChunks: {
-      chunks: "initial",
+      chunks: 'initial',
       minSize: 30000,
       minChunks: 1,
       maxAsyncRequests: 5,
@@ -137,9 +146,9 @@ const config = {
           chunks: 'all',
           enforce: true,
           priority: 20,
-        }
-      }
-    }
+        },
+      },
+    },
   },
 };
 
@@ -150,5 +159,5 @@ if (isDev()) {
 module.exports = config;
 
 function isDev() {
-  return process.env.NODE_ENV === "development";
+  return process.env.NODE_ENV === 'development';
 }
